b367b669a6e7411c19fe52227515fecfeee68784
cjr61 <cjr61@zips.uakron.edu>
2019-05-02 17:49:19 -0400

    removed endogenous control genes

--- Preproc.py
+++ Preproc.py
@@ -3,4 +3,8 @@ import pandas as pd
 
 trainingFrame = pd.read_csv("trainData.tsv", sep='\t')
 
+
+trainingFrame.columns = trainingFrame.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+trainingFrame = trainingFrame[~trainingFrame.gene_description.str.contains("(endogenous control)")]
+
 print(trainingFrame)
