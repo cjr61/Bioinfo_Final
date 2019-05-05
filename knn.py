e6ecab3906e6070e12ddda85ed88d8f227160c9a
cjr61 <cjr61@zips.uakron.edu>
2019-05-05 19:53:30 -0400

    removed unused code

--- knn.py
+++ knn.py
@@ -1,9 +1,5 @@
-import numpy as np
-from sklearn import datasets
 import pandas as pd
-from sklearn.cluster import KMeans
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
 
 knn = KNeighborsClassifier(algorithm='auto', leaf_size=30,
                            metric='minkowski', metric_params=None, n_jobs=1,
@@ -17,9 +13,6 @@ flipped = df.transpose()
 values = flipped[0].values
 flipped = flipped.drop(columns=[0])
 
-# print(flipped.head())
-# print(values)
-
 knn.fit(flipped, values)
 
 tf = pd.read_csv("top_fifty_test.csv")
@@ -29,9 +22,6 @@ tf = tf.drop(columns=['p-value'])
 tf = tf.transpose()
 testValues = tf[0].values
 tf = tf.drop(columns=[0])
-# print(tf.head())
-# tf = tf.drop(tf.ix[:, 53:].columns, axis = 1)
-# print(trueValues)
 
 print("Predicted:")
 print(knn.predict(tf))
